023618cb1e4ce3803b3116cc2499ac4d24089fdf
Quinten <Quinten.pohl@gmail.com>
2019-02-24 13:14:31 +0000

    Project template
    Initial commit for proj creation

---
+++ CmhCrud/Controllers/LinkController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CmhCrud.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LinkController : ControllerBase
+    {
+        // GET: api/Link
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Link/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Link
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Link/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
